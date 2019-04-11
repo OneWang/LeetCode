64a7e7e3b4547f5990a7b7f4aeb66e6c6efd7647
wangqingfei <wangqingfei@exinlei.com>
2019-04-11 22:26:16 +0800

    initialize

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by Jack on 2019/4/11.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
