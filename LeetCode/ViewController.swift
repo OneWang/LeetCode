3026be050a19c05457904176b65571889513e7b4
wangqingfei <wangqingfei@exinlei.com>
2019-04-13 22:27:05 +0800

    add

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("\(bubbleSort(array: [5,2,9,1,7,3]))")
     }
 
     func bubbleSort(array : Array<Int>) -> Array<Int>  {
         var temp = Array<Int>()
+        
         for index in array {
             print("\(index)")
             temp.append(index)
