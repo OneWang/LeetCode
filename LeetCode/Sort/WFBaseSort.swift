eccb0a3e16474962677f45b454cce7eec4f6b354
wangqingfei <wangqingfei@exinlei.com>
2019-04-14 22:25:37 +0800

    add

--- LeetCode/Sort/WFBaseSort.swift
+++ LeetCode/Sort/WFBaseSort.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class WFBaseSort: NSObject {
     static func bubbleSort(array : Array<Int>) -> Array<Int>  {
+        var arr = array
+        arr = arr.sorted()
         var temp = Array<Int>()
-        
         for index in array {
             print("\(index)")
             temp.append(index)
@@ -19,3 +20,26 @@ class WFBaseSort: NSObject {
         return temp
     }
 }
+
+class ArrayPartitionI {
+    func arrayPairSum(_ nums: [Int]) -> Int {
+        var arr = nums
+        arr = arr.sorted()
+        var res = 0
+        for i in 0..<arr.count {
+            if i & 1 != 0 {
+                res += min(arr[i], arr[i - 1])
+            }
+        }
+        return res
+    }
+}
+
+class ArrayPartitionI_2 {
+    func arrayPairSum(_ nums: [Int]) -> Int {
+        return nums.sorted(by: <).enumerated()
+            .compactMap { $0 % 2 == 0 ? $1 : nil }
+            .reduce(0) { $0 + $1 }
+    }
+}
+
